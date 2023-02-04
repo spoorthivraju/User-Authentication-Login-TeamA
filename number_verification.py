76f447743d478c5eabb602fe70b81880b538a02d
Shifali U <54653498+ushifali@users.noreply.github.com>
2023-02-04 14:59:44 +0530

    Add files via upload

---
+++ number_verification.py
@@ -0,0 +1,24 @@
+# Download the helper library from https://www.twilio.com/docs/python/install
+import os
+from twilio.rest import Client
+
+# Set environment variables for your credentials
+# Read more at http://twil.io/secure
+account_sid = "AC97cf18eb8502c798a0ed417237337639"
+auth_token = "3ad8745b026e6a3665076ca5375e23cd"
+verify_sid = "VA535e6667e2431535599fb1a221054ca8"
+verified_number = "+916350193596"
+
+client = Client(account_sid, auth_token)
+
+verification = client.verify.v2.services(verify_sid) \
+  .verifications \
+  .create(to=verified_number, channel="sms")
+print(verification.status)
+
+otp_code = input("Please enter the OTP:")
+
+verification_check = client.verify.v2.services(verify_sid) \
+  .verification_checks \
+  .create(to=verified_number, code=otp_code)
+print(verification_check.status)
