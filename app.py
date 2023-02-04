ddb8d48c089b61885ee748f4d2bfce0e041ac103
Manognya Singuru <73352576+Manognya5@users.noreply.github.com>
2023-02-04 23:46:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ import psycopg2
 from flask import Flask, render_template, request, redirect, url_for, render_template, session, send_from_directory, send_file, flash, abort, make_response
 import decimal
 import itertools 
+from otp import send_email, generateOtp
+
 app = Flask(__name__)
 #Can use this to connect it to our database
 try:
-    db = psycopg2.connect(database="team_a", user = "postgres", password="your_postgres_pswd")
+    db = psycopg2.connect(database="team_a", user = "postgres", password="Srirama!1")
 except:
     print("not connected")
     exit()
@@ -18,10 +20,38 @@ cur = db.cursor()
 @app.route('/home', methods=['POST', 'GET'])
 def logout():
     return render_template("sample.html")
+
 @app.route('/register', methods=['GET'])
 def register():
     if request.method == "GET":
         return render_template("register.html")
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+
+@app.route('/otp', methods=['GET', 'POST'])
+def otp():
+    if request.method == "GET":
+        email = session['email']
+        send_email(email)
+        return render_template("sample.html", msg="sent email to" + email)
+        return render_template("otp.html")
+
+@app.route('/after-otp', methods=['GET', 'POST'])
+def after_otp():
+    if request.method == "POST":
+        email = session['email']
+        otp = session['otp']
+        entered_otp = request.form['otp']
+        print(otp, entered_otp, type(entered_otp), type(otp))
+        if (otp == entered_otp):
+            return render_template("sample.html", msg="success")
+        return render_template("sample.html", msg="sent email to" + email)
+
+
+
 @app.route('/registerUser', methods = ['POST'])
 def registerCustomer():
     if request.method == 'POST':
@@ -50,9 +80,34 @@ def registerCustomer():
             #cur.execute("INSERT INTO CART VALUES(%s, %s);", (cid, cid))
             #db.commit()
             #After Registration move to homepage
-            return redirect('/home')
+            return render_template("login.html", msg="login to continue")
         else:
             return render_template("error.html", error="email/phone number already exists!")
+
+@app.route('/after-login', methods=['POST'])
+def after_login():
+    if request.method == "POST":
+        uname = request.form['cuname']
+        pswd = request.form['cpassword']
+        print(pswd, uname)
+        #Customer Validation
+        cur.execute("SELECT email, userid, pswd from USERS WHERE username = %s", (uname, ))
+        row = cur.fetchall() #row = [('password')]
+        row = list(itertools.chain(*row))
+        print(row[2], pswd, type(row[0]), type(pswd))
+        if row[2] == pswd:
+                session['id'] = row[1]
+                session['email'] = row[0]
+                email = session['email']
+                print(email)
+                otp = generateOtp()
+                session['otp'] = otp
+                send_email(email, otp)
+                return render_template("otp.html", msg="sent email to " + email)
+                return redirect("/otp") #changed
+        print("incorrect")
+        return render_template("login.html", msg="Incorrect password!!")
+        
 
 if __name__ == "__main__":
     app.secret_key = 'secret'
