f21f29d2ad024a52e1d6db1bbd6f978dc136a945
kartikchandna2 <100519293+kartikchandna2@users.noreply.github.com>
2023-02-06 03:40:55 +0530

    Add files via upload

---
+++ create.sql
@@ -0,0 +1,31 @@
+drop database team_a;
+create database team_a;
+
+\c team_a
+
+CREATE TABLE USERS (
+	userid INT,	
+	username VARCHAR(15) NOT NULL,
+	pswd VARCHAR(30) NOT NULL, 
+	fname VARCHAR(15) NOT NULL,
+	lname VARCHAR(15) NOT NULL UNIQUE,
+	phonenumber VARCHAR(10) UNIQUE,
+    email VARCHAR(30) UNIQUE,
+	PRIMARY KEY (userid)
+);
+
+CREATE TABLE QUESTIONS (
+    userid int,
+	qid INT,	
+    answer varchar(30) not null,
+	hint varchar(100) not null,
+	PRIMARY KEY (userid, qid),
+    FOREIGN KEY (userid) REFERENCES USERS(userid)  
+);
+
+CREATE TABLE CUST_OTP (
+    userid int,
+    otp varchar(6) not null,
+    PRIMARY KEY(userid),
+    FOREIGN KEY (userid) REFERENCES USERS(userid)
+);
---
+++ security_questions.py
@@ -0,0 +1,48 @@
+# Define a list of security questions and answers
+import random
+que=['What is your mother\'s maiden name?','What was the name of your first pet?','What is your favorite color?',
+'Where was your best family vacation as a kid?','When you were young, what did you want to be when you grew up?']
+security_questions =  {1: 'What is your mother\'s maiden name?', 2: 'What was the name of your first pet?', \
+    3: 'What is your favorite color?', 4:  'Where was your best family vacation as a kid?', 5:  'When you were young, what did you want to be when you grew up?'}
+
+
+def questions():
+    return que
+# Function to verify the answer to a security question
+def verify_answer(question, answer):
+    for item in security_questions:
+        if item['question'] == question and item['answer'] == answer:
+            return True
+    return False
+
+def get_random_ques():
+    random_ques = random.sample(range(1, 6), 3)
+    d = {}
+    for i in random_ques:
+        d[i] = security_questions[i]
+    return d
+
+
+# Main function to implement two-factor authentication
+def two_factor_auth(username, password):
+    # Verify username and password
+    if username == 'user123' and password == 'pass123':
+        # Prompt user to answer a security question
+        print('Login Successful')
+        for i in range(0,3):
+            l=[]
+            r=random.choice(security_questions)
+            if r not in l:
+                print('Hint for the below Question is ',r['hint'])
+                answer = input(r["question"])
+                if verify_answer(r['question'], answer):
+                    print('Access granted!')
+                    l.append(r)
+                else:
+                    print('Wrong Answer!..Access denied!')
+                    return
+            else:
+                i-=1
+    else:
+        print('Wrong Credentials!..Access denied!')
+
