067d94d2879c39512f3f12a4b6226e38d4f0f3d5
Subhasri Srinivasan <78336130+SubhasriSrinivasan@users.noreply.github.com>
2023-02-07 10:50:27 +0530

    imported config.json

--- number_verification.py
+++ number_verification.py
@@ -5,9 +5,9 @@ from twilio.rest import Client
 # Set environment variables for your credentials
 # Read more at http://twil.io/secure
 def mobile_otp(verified_number):
-  account_sid = "AC129181ef3caec9a6fb8e6d67a726f3a5"
-  auth_token = "b58b7cd1f536f449332722492bc7102b"
-  verify_sid = "VA3e4e885e437cc75d19f7d1a4e736d996"
+  account_sid = appConf['ph_no_cred']['account_sid']
+  auth_token = appConf['ph_no_cred']['auth_token']
+  verify_sid = appConf['ph_no_cred']['verify_sid']
   #verified_number = "+916350193596"
   client = Client(account_sid, auth_token)
   verification = client.verify.v2.services(verify_sid) \
@@ -15,9 +15,9 @@ def mobile_otp(verified_number):
     .create(to="+91"+verified_number, channel="sms")
   print(verification.status)
 def check_mobile_otp(verified_number,otp_code):
-  account_sid = "AC129181ef3caec9a6fb8e6d67a726f3a5"
-  auth_token = "b58b7cd1f536f449332722492bc7102b"
-  verify_sid = "VA3e4e885e437cc75d19f7d1a4e736d996"
+  account_sid = appConf['ph_no_cred']['account_sid']
+  auth_token = appConf['ph_no_cred']['auth_token']
+  verify_sid = appConf['ph_no_cred']['verify_sid']
   client = Client(account_sid, auth_token)
 
   verification_check = client.verify.v2.services(verify_sid) \
