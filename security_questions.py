e59cc2820123960bbde052601e239848419589ab
Manognya Singuru <73352576+Manognya5@users.noreply.github.com>
2023-02-07 10:40:31 +0530

    added a list security questions

--- security_questions.py
+++ security_questions.py
@@ -1,12 +1,12 @@
 # Define a list of security questions and answers
 import random
-
+security_question = ['What is your mother\'s maiden name?', 'What was the name of your first pet?', 'What is your favorite color?',  'Where was your best family vacation as a kid?', 'When you were young, what did you want to be when you grew up?']
 security_questions =  {1: 'What is your mother\'s maiden name?', 2: 'What was the name of your first pet?', \
     3: 'What is your favorite color?', 4:  'Where was your best family vacation as a kid?', 5:  'When you were young, what did you want to be when you grew up?'}
 
 
 def get_questions():
-    return security_questions.values()
+    return security_question
 
 def get_random_questions():
     random_ques = random.sample(range(1, 6), 3)
